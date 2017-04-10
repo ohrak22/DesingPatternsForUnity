020e247fb1e1cc926423ef486bc2af28552f05d1
ohrak22gmail <ohrak22@gmail.com>
2017-04-10 14:37:07 +0800

    SingletonPattern
    싱글톤 패턴

---
+++ DesingPatterns/Assets/Scripts/SingletonPattern/MySingleton.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Singleton
+{
+	// Component type.
+	public class MySingleton : MonoBehaviour
+	{
+		private static MySingleton instance;
+
+		public static MySingleton Instance
+		{
+			get
+			{
+				if(instance == null)
+				{
+					GameObject go = new GameObject("MySingleton");
+					instance = go.AddComponent<MySingleton>();
+					DontDestroyOnLoad(go);
+				}
+				return instance;
+			}
+		}
+	}
+
+	// Non component type.
+	public class Singleton
+	{
+		private static Singleton instance;
+
+		public static Singleton Instance
+		{
+			get
+			{
+				if(instance == null)
+				{
+					instance = new Singleton();
+				}
+				return instance;
+			}
+		}
+	}
+}
---
+++ DesingPatterns/Assets/Scripts/SingletonPattern/SingletonSample.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Singleton
+{
+	public class SingletonSample : MonoBehaviour
+	{
+
+		void Start()
+		{
+			Debug.Log(MySingleton.Instance.name);
+		}
+
+	} 
+}
