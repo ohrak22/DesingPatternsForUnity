92f35c70ed2bf531a23c6154851e77cf0c20ec76
ohrak22gmail <ohrak22@gmail.com>
2017-04-11 13:41:03 +0800

    TemplateMethodPattern
    탬플릿 메소드 패턴

---
+++ DesingPatterns/Assets/Scripts/TemplateMethodPattern/TemplateMethodSample.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace TemplateMethod
+{
+	public class TemplateMethodSample : MonoBehaviour
+	{
+		void Start()
+		{
+			Unit marine = new Marine();
+			Unit firebat = new Firebat();
+
+			marine.EquipWeapon();
+			marine.EquipArmor();
+			marine.Attack();
+			marine.Move();
+
+			firebat.EquipWeapon();
+			firebat.EquipArmor();
+			firebat.Attack();
+			firebat.Move();
+		}
+	} 
+}
---
+++ DesingPatterns/Assets/Scripts/TemplateMethodPattern/Unit.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace TemplateMethod
+{
+	public abstract class Unit
+	{
+		public abstract void EquipWeapon();
+		public abstract void Attack();
+
+		public void EquipArmor()
+		{
+			Debug.Log("Equip armor");
+		}
+		public void Move()
+		{
+			Debug.Log("Move");
+		}
+
+	}
+
+	public class Marine : Unit
+	{
+		public override void EquipWeapon()
+		{
+			Debug.Log("Equip rifle");
+		}
+		public override void Attack()
+		{
+			Debug.Log("Shoot!");
+		}
+	}
+
+	public class Firebat : Unit
+	{
+		public override void EquipWeapon()
+		{
+			Debug.Log("Equip flamethrower");
+		}
+		public override void Attack()
+		{
+			Debug.Log("Fire!");
+		}
+	}
+}
