66f64662dd388310cd3ebd14484f27a3f06ca0d6
ohrak22gmail <ohrak22@gmail.com>
2017-04-11 15:04:49 +0800

    StatePattern
    스테이트 패턴

---
+++ DesingPatterns/Assets/Scripts/StatePattern/StateSample.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace State
+{
+	public class StateSample : MonoBehaviour
+	{
+		void Start()
+		{
+			Unit unit = new Unit();
+			unit.Move();
+			unit.Stop();
+			unit.Attack();
+			unit.UnderAttack();
+			unit.Attack();
+		}
+		
+	} 
+}
---
+++ DesingPatterns/Assets/Scripts/StatePattern/Unit.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace State
+{
+	public interface State
+	{
+		void Print();			 
+	}
+
+	public class IdleState : State
+	{
+		public void Print()
+		{
+			Debug.Log(GetType().Name);
+		}
+	}
+	public class AttackState : State
+	{
+		public void Print()
+		{
+			Debug.Log(GetType().Name);
+		}
+	}
+	public class MoveState : State
+	{
+		public void Print()
+		{
+			Debug.Log(GetType().Name);
+		}
+	}
+	public class UnderAttackState : State
+	{
+		public void Print()
+		{
+			Debug.Log(GetType().Name);
+		}
+	}
+
+	public class Unit
+	{
+		private State Idle = new IdleState();
+		private State AttackState = new AttackState();
+		private State MoveState = new MoveState();
+		private State UnderAttackState = new UnderAttackState();
+		public State state { get; set; }
+
+		public void Stop()
+		{
+			state = Idle;
+			state.Print();
+		}
+		public void Attack()
+		{
+			state = AttackState;
+			state.Print();
+		}
+		public void Move()
+		{
+			state = MoveState;
+			state.Print();
+		}
+		public void UnderAttack()
+		{
+			state = UnderAttackState;
+			state.Print();
+		}
+	} 
+}
