bd68f374a5c31ba11c7b5896f7c2a0da2be391bb
ohrak22gmail <ohrak22@gmail.com>
2017-04-11 11:45:52 +0800

    FacadePattern
    퍼사드 패턴

---
+++ DesingPatterns/Assets/Scripts/FacadePattern/FacadeSample.cs
@@ -0,0 +1,17 @@
+﻿namespace Facade
+{
+	using UnityEngine;
+	using System.Collections;
+
+	public class FacadeSample : MonoBehaviour
+	{
+		void Start()
+		{
+			GameObject go = new GameObject("Factory");
+			Factory factory = go.AddComponent<Factory>();
+			factory.CreateTank();
+		}
+
+	}
+
+}
---
+++ DesingPatterns/Assets/Scripts/FacadePattern/Factory.cs
@@ -0,0 +1,62 @@
+﻿namespace Facade
+{
+	using UnityEngine;
+	using System.Collections;
+
+	public class Factory : MonoBehaviour
+	{
+		public int mineal = 200;
+		public int gas = 100;
+		public int enoughPeopleCount = 5;
+
+		public void CreateTank()
+		{
+			if(!IsEnoughMineal())
+			{
+				Debug.Log("Not enough mineal!");
+			}
+			else if(!IsEnoughGas())
+			{
+				Debug.Log("Not enough gas!");
+			}
+			else if(!IsEnoughPeopleCount())
+			{
+				Debug.Log("Not enough supplies!");
+			}
+			else
+			{
+				StartCoroutine(Process());
+			}
+		}
+		public bool IsEnoughMineal()
+		{
+			return mineal >= 150;
+		}
+		public bool IsEnoughGas()
+		{
+			return gas >= 100;
+		}
+		public bool IsEnoughPeopleCount()
+		{
+			return enoughPeopleCount >= 2;
+		}
+
+		IEnumerator Process()
+		{
+			mineal -= 150;
+			gas -= 100;
+			enoughPeopleCount -= 2;
+
+			for(int i = 0; i <= 100; i += 10)
+			{
+				Debug.Log(i + "%");
+				yield return new WaitForSeconds(0.1f);
+			}
+
+			GameObject tank = new GameObject("Tank");
+			Debug.Log("Creation completed!");
+		}
+
+	}
+
+}
